457d6adf74cc3f89facc00ee707ee364c1f604fc
Ian Wilson <apple@smallbusinesswebservices.co.uk>
2014-10-11 00:08:46 +0100

    modified completed solution lecture 142 challenge

--- User Data Challenge Solution/ViewController.m
+++ User Data Challenge Solution/ViewController.m
@@ -20,11 +20,31 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.users = [UDUserData users];
     NSLog(@"%@",self.users);
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"userCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    NSDictionary *user = self.users[indexPath.row];
+    cell.textLabel.text = user[USERNAME];
+    cell.detailTextLabel.text = user[EMAIL];
+    cell.imageView.image = user[PROFILE_PICTURE];
+    
+    return cell;
+    
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.users count];
+}
+
 
 @end
